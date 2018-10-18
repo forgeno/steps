76c65d460c25dfa2771a6cfd4c4a757a63a35fd2
Sidkhanna96 <31675687+Sidkhanna96@users.noreply.github.com>
2018-10-17 21:04:06 -0600

    Django Integration. Please do not merge. (#49)
    * Django
    
    * requirements modify
    
    * Allowed Hosts
    
    * Basic
    
    * ip
    
    * two ip
    
    * Superuser
    
    * Apache-Config for CD
    
    * Webpage compatibility
    
    * Database and JS Testing
    
    * Henry Code
    
    * Removed Files
    
    * test

---
+++ django_steps/frontend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+# from .views import AboutPage
+from . import views
+from django.urls import include, path
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.index),
+    path('about/', views.about_page),
+    path('statistics/', views.statistics_page),
+]
