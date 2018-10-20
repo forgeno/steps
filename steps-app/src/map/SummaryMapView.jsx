767a080ddf0b86ddfe8ee830c9b66f50d57f12e1
machung21 <machung@ualberta.ca>
2018-10-20 13:53:33 -0600

    overlayed drawing layer using ivans dev acc (#57)
    * overlayed drawing layer using ivans dev acc
    
    * modified redundancy for constants in summaryMap.jsx

--- steps-app/src/map/SummaryMapView.jsx
+++ steps-app/src/map/SummaryMapView.jsx
@@ -18,7 +18,59 @@ export default class SummaryMapView extends Component {
 	  }
 
 	loadOSMBaseMap() {
-		esriLoader.loadModules(['esri/Map', 'esri/views/MapView'], esriURL).then((Actions.displayOSMBaseMap))
+		esriLoader.loadModules([ 'esri/Map', 
+		'esri/views/MapView', 
+		"esri/layers/FeatureLayer", 
+		"esri/layers/CSVLayer", 
+		"esri/Color",
+		"esri/symbols/SimpleMarkerSymbol", 
+		"esri/renderers/SimpleRenderer"], esriURL).then(([Map, MapView, FeatureLayer, CSVLayer, Color, SimpleMarkerSymbol, SimpleRenderer]) => {
+				let map = new Map({
+					basemap: "osm"
+				});
+
+				const featureLayer = new FeatureLayer({
+				 	url: "https://services9.arcgis.com/8ccGcFm2KpUhl0DB/arcgis/rest/services/edm_network_walkability/FeatureServer"
+				});
+
+				 map.add(featureLayer);
+			
+				const csvLayer = new CSVLayer({
+					url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv"
+				});
+				   //var orangeRed = new Color([238, 69, 0, 0.5]); // hex is #ff4500
+				   //var marker = new SimpleMarkerSymbol("solid", 15, null, orangeRed);
+				   //var renderer = new SimpleRenderer(marker);
+				   //csvLayer.setRenderer(renderer);
+				   csvLayer.renderer = {
+					type: "simple",  // autocasts as new SimpleRenderer()
+					symbol: {
+					  type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+					  size: 2,
+					  color: "red",
+					  outline: {  // autocasts as new SimpleLineSymbol()
+						width: 0.5,
+						color: "white"
+					  }
+					}
+				};
+				 map.add(csvLayer);
+
+				let view = new MapView({
+					map: map,
+					container: "mapContainer",
+					basemap: 'osm',
+					center: [-113.4990, 53.5405],
+					zoom: 15
+				});
+	
+				this.setState({
+					map,
+					view
+				})
+
+				})
+
 				.catch(err => {
 				console.error(err);
 			});
