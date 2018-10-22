3fb24aeaf0a646c654633c37d2c732410b8236bf
Adil Malik <619mwa@gmail.com>
2018-10-22 10:25:16 -0600

    First 7 endpoints finished (#60)
    * start on endpoints for sidewalk
    
    * test query
    
    * sidewalk summary endpoints done
    
    * test commit
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * space to tab
    
    * t
    
    * space to tab
    
    * stuff
    
    * stuff
    
    * space to tab
    
    * t
    
    * space to tab
    
    * format
    
    * format
    
    * format
    
    * format
    
    * format
    
    * format
    
    * fixed tuple
    
    * deleted files
    
    * Update AboutPageView.jsx

--- django_steps/api/serializers.py
+++ django_steps/api/serializers.py
@@ -1,33 +1,40 @@
 from rest_framework_json_api import serializers
-from .models import Sidewalk, SidewalkRating, Pedestrian, SidewalkComment, SidewalkImage, AdminAccount
+from .models import Sidewalk, Pedestrian, SidewalkComment, SidewalkImage, AdminAccount
 
 class SidewalkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sidewalk
-        fields = ('id',)
+	class Meta:
+		model = Sidewalk
+		fields = '__all__'
 
-    
-class SidewalkRatingsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SidewalkRating
-        fields = '__all__'
+class SidewalkListSerializer(serializers.ModelSerializer):
+	accessibility = serializers.FloatField()
+	comfort = serializers.FloatField()
+	connectivity = serializers.FloatField()
+	senseOfSecurity = serializers.FloatField()
+	physicalSafety = serializers.FloatField()
+	class Meta:
+		model = Sidewalk
+		fields = ('id', 'accessibility', 'comfort', 'connectivity', 'senseOfSecurity', 'physicalSafety')
 
 class PedestrianSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pedestrian
-        fields = '__all__'
-
-class SidewalkCommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SidewalkComment
-        fields = '__all__'
+	class Meta:
+		model = Pedestrian
+		fields = '__all__'
 
 class SidewalkImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SidewalkImage
-        fields = '__all__'
+	url = serializers.CharField(source='image_url')
+	date = serializers.DateTimeField(source='posted_time')
+	class Meta:
+		model = SidewalkImage
+		fields = ('id', 'url', 'date')
+
+class SidewalkCommentSerializer(serializers.ModelSerializer):
+	date = serializers.DateTimeField(source='posted_time')
+	class Meta:
+		model = SidewalkComment
+		fields = ('id', 'text', 'date')
 
 class AdminAccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AdminAccount
-        fields = '__all__'
+	class Meta:
+		model = AdminAccount
+		fields = '__all__'
--- django_steps/api/urls.py
+++ django_steps/api/urls.py
@@ -3,11 +3,8 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('sidewalk', views.SidewalkView)
-router.register('sidewalkRating', views.SidewalkRatingView)
+router.register('sidewalk', views.SidewalkView, 'sidewalk')
 router.register('pedestrian', views.PedestrianView)
-router.register('sidewalkComment', views.SidewalkCommentView)
-router.register('sidewalkImage', views.SidewalkImageView)
 router.register('adminAccount', views.AdminAccountView)
 
 urlpatterns = [
