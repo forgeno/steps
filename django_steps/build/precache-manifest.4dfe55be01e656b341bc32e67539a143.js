7c207822fc8e289e5bd41da3036e457552887db0
htruong1 <htruong1@ualberta.ca>
2018-10-24 18:53:04 -0600

    moved build

---
+++ django_steps/build/precache-manifest.4dfe55be01e656b341bc32e67539a143.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "3c9db6d89f39280e2721",
+    "url": "/static/css/main.386d595c.chunk.css"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "21bebebae23921c48d78",
+    "url": "/static/js/1.21bebeba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c9db6d89f39280e2721",
+    "url": "/static/js/main.3c9db6d8.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "21bebebae23921c48d78",
+    "url": "/static/css/1.191b9d0c.chunk.css"
+  },
+  {
+    "revision": "d222146dc20b9a0bf28840c33bfaf3b0",
+    "url": "/index.html"
+  }
+];
