c2ce5baf5ebdcf03d12d5b1357877572415942c2
Adil Malik <619mwa@gmail.com>
2018-11-25 16:58:02 -0700

    Build for sprint 4 progress (#142)
    * chmod in deploy script
    
    * build for deploy

--- scripts/deployCybera.sh
+++ scripts/deployCybera.sh
@@ -3,5 +3,6 @@ git fetch
 git pull origin master
 rm -rf /var/www/html/steps
 cp -R django_steps /var/www/html/steps
+chmod -R 777 /var/www/html/steps
 sudo service apache2 restart
 echo "Finished deploying server"
