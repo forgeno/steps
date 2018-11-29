890e177b12a70ab2d12f3127735a2af64a1c0fc0
Matthew Chung <machung@ualberta.ca>
2018-11-28 22:49:42 -0700

    removed duplicated move build process from cybera script since this is in local script already

--- scripts/deployCybera.sh
+++ scripts/deployCybera.sh
@@ -2,8 +2,6 @@ echo "Running persistant server"
 git fetch
 git pull origin master
 rm -rf /var/www/html/steps
-rm -rf django_steps/build
-mv steps-app/build django_steps/build
 cp -R django_steps /var/www/html/steps
 chmod -R 777 /var/www/html/steps
 sudo service apache2 restart
